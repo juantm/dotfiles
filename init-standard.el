b7e234d03c30fc7b357067ec18c9fc9eaa95a872
Juan E. Toro <jetoro@falabella.cl>
2023-07-13 11:31:33 -0400

    Se agrega treemacs y which-key

--- init-standard.el
+++ init-standard.el
@@ -21,20 +21,30 @@
 ;; (set-face-attribute 'default nil :font "Cascadia Code" :height 120)
 (set-face-attribute 'default nil :font "IntelOne Mono" :height 120 :weight 'regular)
 (when (eq system-type 'darwin)
-  (set-face-attribute 'default nil :font "IntelOne Mono" :height 140 :weight 'regular))
+  ;; (set-face-attribute 'default nil :font "IntelOne Mono" :height 160 :weight 'regular)
+  (set-face-attribute 'default nil :font "JetBrainsMonoNL NF" :height 150)
+  ;; (set-face-attribute 'default nil :font "CozetteVector" :height 150)
+  ;; (set-face-attribute 'default nil :font "Envy Code R" :height 150)
+  ;; (set-face-attribute 'default nil :font "Ubuntu" :height 150)
+  )
 (load-theme 'adwaita t)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
-(electric-pair-mode 1)
+(electric-pair-mode t)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
 (unless (package-installed-p 'vertico)
   (package-install 'vertico))
+(use-package vertico
+  :init
+  (vertico-mode t)
+  )
 
-(require 'vertico)
-(vertico-mode t)
 (unless (package-installed-p 'consult)
   (package-install 'consult))
+
 (setq read-buffer-completion-ignore-case t
       read-file-name-completion-ignore-case t
       completion-ignore-case t)
@@ -42,14 +52,34 @@
 ;;; Pop-up completion
 (unless (package-installed-p 'corfu)
   (package-install 'corfu))
-
-;; Enable autocompletion by default in programming buffers
-(add-hook 'prog-mode-hook #'corfu-mode)
+(use-package corfu
+  :custom
+  (corfu-auto t)
+  :init
+  (global-corfu-mode))
 
 ;;; Git client
 (unless (package-installed-p 'magit)
   (package-install 'magit))
 
+
+(use-package treemacs
+  :ensure t
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-c t 1"   . treemacs-delete-other-windows)
+        ("C-c t t"   . treemacs)
+        ("C-c t d"   . treemacs-select-directory)
+        ("C-c t B"   . treemacs-bookmark)
+        ("C-c t C-t" . treemacs-find-file)
+        ("C-c t M-t" . treemacs-find-tag)))  
+  )
+
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode t))
 
 (setq-default major-mode
               (lambda () ; guess major mode from file name
