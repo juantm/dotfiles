4b72f5ebb6eb2ce8e4d51efb1224bfca4ba501dc
Juan E. Toro <jetoro@falabella.cl>
2024-02-22 20:16:45 -0300

    Se corrige error de nombre de modulo

--- nvim/.config/nvim/lua/plugins/nvim-surround.lua
+++ nvim/.config/nvim/lua/plugins/nvim-surround.lua
@@ -4,7 +4,7 @@ return {
 		version = "*", -- Use for stability; omit to use `main` branch for the latest features
 		event = "VeryLazy",
 		config = function()
-			require("nvm-surround").setup({
+			require("nvim-surround").setup({
 				-- Configuration here, or leave empty to use defaults
 			})
 		end,
