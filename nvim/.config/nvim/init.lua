643cdfd7a537c91e170a8d4e6e2cec821dbcbf56
Juan E. Toro <jetoro@falabella.cl>
2024-02-22 23:25:50 -0300

    Se agregan configuraciones de indenacion y keymap de tabs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,7 +6,10 @@ set.expandtab = true
 set.number = true
 set.relativenumber = true
 set.foldcolumn = "3"
-set.autoindent = true
+set.smartindent = true
+set.incsearch = true
+set.hlsearch = true
+set.mouse = "a"
 
 vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -30,3 +33,7 @@ vim.keymap.set('n', '<leader>bb', ':ls<CR>')
 vim.keymap.set('n', '<leader>bd', ':bd<CR>')
 vim.keymap.set('n', '<leader>fs', ':w<CR>')
 vim.keymap.set('n', '<leader>qq', ':qa!<CR>')
+vim.keymap.set('n', '<leader>tn', ':tabNext<CR>')
+vim.keymap.set('n', '<leader>tp', ':tabPrevious<CR>')
+vim.keymap.set('n', '<leader>td', ':tabclose<CR>')
+
