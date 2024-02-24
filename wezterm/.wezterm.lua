2a397a54a0072b33de0dff1f447cb06e85220e27
Juan E Toro <juan.toro.marty@gmail.com>
2024-02-24 14:57:23 -0300

    Se cambia colores segun SO

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,19 +7,26 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 -- config.font = wezterm.font_with_fallback {'Cozette', 'Iosevka Term SS07', 'MesloLGS NF'}
 -- config.font = wezterm.font_with_fallback { 'Terminus (TTF)', 'Terminess Nerd Font', 'MesloLGS NF'}
-config.font = wezterm.font_with_fallback { "Iosevka Term SS07", "IosevkaTerm Nerd Font" }
+config.font = wezterm.font_with_fallback({ "Iosevka Term SS07", "IosevkaTerm Nerd Font" })
 config.font_size = 16.0
 
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Frappe"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
 -- For example, changing the color scheme:
-config.color_scheme = 'Catppuccin Frappe'
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 -- and finally, return the configuration to wezterm
 -- config.default_prog = { '/usr/local/bin/tmux', '-2' }
 config.hide_tab_bar_if_only_one_tab = true
 return config
-
