0276c84e3b55d3127817c7c0a26f47038760cdfe
Juan E. Toro <jetoro@falabella.cl>
2024-03-05 19:46:44 -0300

    Se agrega configuracion de catppuccin para mejor integracion

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,12 +1,22 @@
 return {
-	"catppuccin/nvim",
-	lazy = false,
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "frappe",
-			show_end_of_buffer = true,
-		})
-	end,
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "frappe",
+      show_end_of_buffer = true,
+      integrations = {
+        neotree = true,
+        barbar = true,
+        telescope = {
+          enabled = true,
+          style = "nvchad"
+        },
+        gitgutter =true,
+      },
+    })
+--    vim.cmd.colorscheme("catppuccin")
+  end,
 }
