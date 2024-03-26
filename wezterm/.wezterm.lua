62276028181c7e99366e6e7925fab5e2665d6539
Juan E. Toro <jetoro@falabella.cl>
2024-03-26 12:28:45 -0300

    Cambios varios

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -17,13 +17,13 @@ end
 config.font = wezterm.font_with_fallback({ "Dank Mono", "DankMono Nerd Font" })
 local font_size = 14.0
 if wezterm.target_triple == 'x86_64-apple-darwin' then
-  font_size = 18.0
+  font_size = 16.0
 end
 config.font_size = font_size
 
 function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
-		return "Catppuccin Frappe"
+		return "Catppuccin Macchiato"
 	else
 		return "Catppuccin Latte"
 	end
