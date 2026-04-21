eb40b8ce82de57f37cc0a1beff170faf3de24941
Juan E. Toro <juan.toro.marty@gmail.com>
2026-04-20 20:45:19 -0400

    Cambios en wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,6 +10,13 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+-- Set initial window size in character cells
+config.initial_rows = 40
+config.initial_cols = 160
+
+config.window_background_opacity = 0.74 -- opacity as you please
+config.kde_window_background_blur = true
+
 -- This is where you actually apply your config choices
 -- config.font = wezterm.font_with_fallback {'Cozette', 'Iosevka Term SS05', 'MesloLGS NF'}
 -- config.font = wezterm.font_with_fallback { 'Terminus (TTF)', 'Terminess Nerd Font', 'MesloLGS NF'}
@@ -21,15 +28,20 @@ fuentes = wezterm.font_with_fallback({
 	--  { family = "Cozette", weight = "Regular" },
 	--- { family = "Victor Mono", weight = 400 },
 	{
+		family = "Consolas ligaturized v3",
 		--  family = "Fira Code iScript",
-		family = "LigaConsola Nerd Font",
+		--  family = "LigaConsola Nerd Font",
 		--  weight = "Light",
 		harfbuzz_features = { "zero", "ss01", "ss02", "ss03", "ss07", "cv24", "cv31" },
 	},
-	{ family = "CaskaydiaCove Nerd Font", weight = "Regular" },
+	{
+		-- family = "CaskaydiaCove Nerd Font",
+		family = "Victor Mono",
+		weight = "Regular",
+	},
 })
 config.font = fuentes
-local font_size = 14.0
+local font_size = 11.0
 if wezterm.target_triple == "x86_64-apple-darwin" then
 	font_size = 16.0
 end
